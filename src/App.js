4a931ce4bf70ff3b9b52e14344c575fbd7c24746
shubham-karad <shubham4986@gmail.com>
2020-11-16 22:31:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">Shopping Cart</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        All Rights Reserved
+      </footer>
+    </div>
+  );
+}
+
+export default App;
