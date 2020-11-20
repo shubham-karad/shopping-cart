85c905ddf897b461331d7c4877afb4596f21fcdc
shubham-karad <shubham4986@gmail.com>
2020-11-21 00:25:00 +0530

    create order

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,6 +3,9 @@ import formatCurrency from '../util';
 import Fade from 'react-reveal/Fade' 
 import { connect } from 'react-redux';
 import { removeFromCart } from '../actions/cartActions';
+import {createOrder, clearOrder} from '../actions/orderActions'
+import Modal from 'react-modal'
+import Zoom from 'react-reveal/Zoom'
 
 class Cart extends Component {
 
@@ -27,20 +30,65 @@ class Cart extends Component {
             name:this.state.name,
             email:this.state.email,
             address:this.state.address,
-            cartItems:this.props.cartItems
+            cartItems:this.props.cartItems,
+            total: this.props.cartItems.reduce((a,c) => a+ c.price * c.count, 0)
         }
 
         this.props.createOrder(order);
     }
 
+    closeModal = () => {
+        this.props.clearOrder()
+    }
+
     render() {
-        const {cartItems} = this.props;
+        const {cartItems, order} = this.props;
         return (
             <div>
                 {cartItems.length === 0 ? <div className="cart cart-header">Cart is Empty</div>
                 :
                 <div className="cart cart-header">You have {cartItems.length} in the cart</div>}
-
+                {
+                    order && <Modal isOpen={true} onRequestClose={this.closeModal}>
+                        <Zoom>
+                            <button className="close-modal" onClick={this.closeModal}>x</button>
+                            <div className="order-details">
+                                <h3 className="success-message">Your order has been placed</h3>
+                                <h2>Order {order._id}</h2>
+                                <ul>
+                                    <li>
+                                        <div>Name:</div>
+                                        <div>{order.name}</div>
+                                    </li>
+                                    <li>
+                                        <div>Email:</div>
+                                        <div>{order.email}</div>
+                                    </li>
+                                    <li>
+                                        <div>Address:</div>
+                                        <div>{order.address}</div>
+                                    </li>
+                                    <li>
+                                        <div>Date:</div>
+                                        <div>{order.createdAt}</div>
+                                    </li>
+                                    <li>
+                                        <div>Total:</div>
+                                        <div>{formatCurrency(order.total)}</div>
+                                    </li>
+                                    <li>
+                                        <div>Cart Items:</div>
+                                        <div>{order.cartItems.map((x) => (
+                                            <div>
+                                                {x.count} {" x "} {x.title}
+                                            </div>
+                                        ))}</div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </Zoom>
+                        </Modal>
+                }
                 <div>
                     <div className="cart">
                         <Fade left cascade>
@@ -110,7 +158,8 @@ class Cart extends Component {
 
 export default connect(
     (state) => ({
+        order: state.order.order,
         cartItems: state.cart.cartItems,
     }),
-    {removeFromCart}
+    {removeFromCart, createOrder, clearOrder}
 )(Cart);
--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
+import { orderReducer } from './reducers/orderReducers';
 import {productsReducer} from './reducers/productReducers';
 
 const initialState = {};
@@ -8,7 +9,8 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     combineReducers({
         products: productsReducer,
-        cart: cartReducer
+        cart: cartReducer,
+        order: orderReducer
     }),
     initialState,
     composeEnhancers(applyMiddleware(thunk))
